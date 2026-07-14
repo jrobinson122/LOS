7d9fd8336dd54a6c6caf2f3845ed1e29d2cad53b
jrobinson122 <jonkrobinson122@gmail.com>
2026-07-13 23:20:36 -0400

    Fixed import issues

--- src/components/background.tsx/PostProcessing.tsx
+++ src/components/background.tsx/PostProcessing.tsx
@@ -1,26 +1,26 @@
-import {
-  Bloom,
-  ChromaticAberration,
-  EffectComposer,
-  Noise,
-  Vignette,
-} from "@react-three/postprocessing";
-import { BlendFunction } from "postprocessing";
+// import {
+//   Bloom,
+//   ChromaticAberration,
+//   EffectComposer,
+//   Noise,
+//   Vignette,
+// } from "@react-three/postprocessing";
+// import { Vector2 } from "three";
+// import { BlendFunction } from "postprocessing";
 
-export default function PostProcessing() {
-  return (
-    <EffectComposer>
-      <Bloom
-        intensity={1.55}
-        luminanceThreshold={0.12}
-        luminanceSmoothing={0.22}
-      />
+// export default function PostProcessing() {
+//   return (
+//     <EffectComposer>
+//       <Bloom
+//         intensity={1.55}
+//         luminanceThreshold={0.12}
+//         luminanceSmoothing={0.22}
+//       />
 
-      <ChromaticAberration offset={[0.0012, 0.0012]} />
+//       <ChromaticAberration offset={new Vector2(0.0012, 0.0012)} />
+//       <Noise opacity={0.08} blendFunction={BlendFunction.SOFT_LIGHT} />
 
-      <Noise opacity={0.08} blendFunction={BlendFunction.SOFT_LIGHT} />
-
-      <Vignette eskil={false} offset={0.25} darkness={0.75} />
-    </EffectComposer>
-  );
-}
+//       <Vignette eskil={false} offset={0.25} darkness={0.75} />
+//     </EffectComposer>
+//   );
+// }
